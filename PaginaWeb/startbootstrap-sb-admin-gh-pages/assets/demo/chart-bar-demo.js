26411ce0a63ca39fcf8a4ed9fae8b15d21f42cb7
Davidrj <davidroldan2901@gmail.com>
2022-01-26 21:35:54 +0100

    web

--- PaginaWeb/startbootstrap-sb-admin-gh-pages/assets/demo/chart-bar-demo.js
+++ PaginaWeb/startbootstrap-sb-admin-gh-pages/assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
     datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      label: "Ventas",
+      backgroundColor: "#009655",
+      borderColor: "#009655",
+      data: [4215, 10312, 6251, 7841, 4000, 14984],
     }],
   },
   options: {
